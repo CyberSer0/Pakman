d0b4cad428cb1a33f13202399360fefad0ded50d
Artur Szałyga <arturszalyga@wp.pl>
2021-10-12 17:57:50 +0200

    SFML added

--- Pakman/main.cpp
+++ Pakman/main.cpp
@@ -1,5 +1,32 @@
-#include <iostream>;
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <iostream>
 
-int main() {
+class Player : public sf::Drawable
+{
+private:
+	virtual void draw(sf::RenderTarget target, sf::RenderStates states) const;
+};
 
+int main() 
+{
+	HWND hwnd = GetConsoleWindow();
+	ShowWindow(hwnd, SW_HIDE);
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Pakman");
+	Player player;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
 }
