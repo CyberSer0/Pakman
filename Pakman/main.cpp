01502313befbd06e6d223f0b5118a76a86d678ec
Artur Szałyga <arturszalyga@wp.pl>
2021-11-22 03:01:51 +0100

    Revert "Main Menu + Scene class"
    This reverts commit df905398304c8778050adcdd2f6306470813be73.

--- Pakman/main.cpp
+++ Pakman/main.cpp
@@ -1,59 +1,19 @@
-#include "MainMenu.h"
 #include "Game.h"
 #include <windows.h>
 
 
-int main()
+int main() 
 {
 	// Init Game
-	MainMenu mainMenu;
-	Game game = Game(mainMenu.getWindow());
+	Game game;
 
 	// Loop
-	do {
-		// Main menu loop when main menu is running
-		if (mainMenu.isRunning())
-		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
-				if (mainMenu.buttonSelected == 1)
-				{
-					mainMenu.menuState = false;
-					game.gameState = true;
-					game.initMap();
-				}
-				else if (mainMenu.buttonSelected == 4)
-				{
-					return 0;
-				}
+	while (game.gameRunning())
+	{
+		// Update
+		game.update();
 
-			// Update logic
-			mainMenu.update();
-
-			// Render graphics
-			mainMenu.render();
-		}
-
-		// Game loop when game is running
-		else if (game.gameRunning())
-		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			{
-				mainMenu.~MainMenu();
-				game.~Game();
-				return 0;
-				/*game.gameState = false;
-				mainMenu.menuState = true;*/
-			}
-
-			// Update logic
-			game.update();
-
-			// Render graphics
-			game.render();
-		}
-		else 
-		{
-			break;
-		}
-	} while (1);
+		// Render
+		game.render();
+	}
 }
