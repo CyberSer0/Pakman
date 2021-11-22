df905398304c8778050adcdd2f6306470813be73
Artur Szałyga <arturszalyga@wp.pl>
2021-11-22 03:01:01 +0100

    Main Menu + Scene class

--- Pakman/Game.h
+++ Pakman/Game.h
@@ -1,32 +1,29 @@
 #pragma once
+#include "Scene.h"
 #include "Entity.h"
 #include "Enemy.h"
 #include "Player.h"
 #include "Tilemap.h"
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include <random>
 
 /*
 	Main Game class/functionality wrapper
 */
 
-class Game
+class Game : protected Scene
 {
 private:
-	// Window
-	sf::RenderWindow* window; // Creating window object
-	sf::VideoMode videoMode; // Creating videoMode object (holds the dimensions and other variables of the displayed window)
-	sf::Event event; // Creating an event handle for Event usability
+	sf::Event event;
 
-	// View
-	sf::View view; // Creating a view object to use as a camera
 
 	// Map
 	Tilemap* map; // Creating a ptr to the tilemap object map for creating a game environment
 	sf::Vector2u tileSize; // Dimensions of one single tile of the tilemap
 	int* level; // Pointer to the array of tiles in the tilemap
 	float halfSize; // Half of the game enviroment length (for view setting)
-	unsigned int size; // Size of the game environment (in nr of tiles)
 
 	// Entities
 	Player* player; // Creating a pointer to a player object
@@ -34,20 +31,25 @@ private:
 
 	// Private functions
 	void initVariables(); // Function to initialize differrent variables
-	void initWindow(); // Function to initialize the window and all it's functionality
-	void initMap(); // Function to initialize and load the game environment map
 	void initView(); // Function to set and modify the camera view
 	sf::View letterBox(sf::View view, int width, int height); // Function to responsively adjust the scale and size of the game objects and view
 	void initPlayer(); // Function to initialize the player object and it's functionality
 
 public: 
+	// Public Variables
+	bool gameState;
+	unsigned int size; // Size of the game environment (in nr of tiles)
 
+	// View
+	sf::View view; // Creating a view object to use as a camera
+	
 	/*
 	* Constructor - used to initialize all functions at the start of the game
 	* Deconstor - deletes objects created in the constructor to prevent memory leaks
 	*/
-	Game(); // Constructor
+	Game(sf::RenderWindow* window); // Constructor
 	virtual ~Game(); // Deconstructor
+	void initMap(); // Function to initialize and load the game environment map
 	
 	/*
 	* Accessors - serve as bools to check certain game states
---
+++ Pakman/MainMenu.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Scene.h"
+#include "Game.h"
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#define MAX_MAINMENU_BUTTONS 5
+#define FONT_SIZE 16
+
+class MainMenu : protected Scene
+{
+// Private Sector
+private:
+
+	sf::Font font;
+	sf::Text mainMenu[MAX_MAINMENU_BUTTONS];
+	sf::Event event;
+
+	sf::RectangleShape selectionRect;
+
+	void initFont();
+	void initVariables();
+	void initView();
+	void initButtons();
+
+	void SelectUp();
+	void SelectDown();
+
+// Public Sector
+public:
+	bool menuState;
+	int buttonSelected;
+
+	sf::View view;
+	
+	MainMenu();
+	virtual ~MainMenu();
+
+	sf::View letterBox(sf::View view, float width, float height);
+
+	void updateEvents();
+	void update();
+	void render();
+
+	int MainMenuPressed();
+
+	const bool isRunning() const;
+	sf::RenderWindow* getWindow();
+};
+
