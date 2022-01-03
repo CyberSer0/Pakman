9b9a2604687ab5054556bfe4785661c65129fdde
Artur Szałyga <arturszalyga@wp.pl>
2022-01-03 11:18:14 +0100

    Revert "Revert "Revert "Revert "Main Menu + Scene class""""
    This reverts commit 4462c45fa723f21ac2698e9711dbf01a80cf7f24.

---
+++ Pakman/Scene.cpp
@@ -0,0 +1,39 @@
+#include "Scene.h"
+
+// Private Methods
+void Scene::initVariables()
+{
+	this->window = nullptr;
+}
+
+void Scene::initWindow()
+{
+	this->videoMode.height = 480;
+	this->videoMode.width = 640;
+
+	this->window = new sf::RenderWindow(this->videoMode, "Pakman");
+	this->window->setFramerateLimit(60);
+	this->window->setVerticalSyncEnabled(true);
+}
+
+// Public Methods
+Scene::Scene()
+{
+	initVariables();
+	initWindow();
+}
+
+Scene::Scene(sf::RenderWindow* window)
+{
+	this->window = window;
+}
+
+Scene::~Scene()
+{
+	delete this->window;
+}
+
+sf::RenderWindow* Scene::getWindow()
+{
+	return this->window;
+}
